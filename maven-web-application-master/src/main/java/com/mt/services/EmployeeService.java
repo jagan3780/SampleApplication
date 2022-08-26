89734bf9229988f4066ddc31ff13bedf1b1869bb
jagan3780 <71879196+jagan3780@users.noreply.github.com>
2022-08-26 16:05:28 +0530

    Update EmployeeService.java
    chnages

--- maven-web-application-master/src/main/java/com/mt/services/EmployeeService.java
+++ maven-web-application-master/src/main/java/com/mt/services/EmployeeService.java
@@ -23,7 +23,7 @@ public class EmployeeService {
 
 		JSONObject js = new JSONObject();
 		js.put("Name", "Cloudz DevOps");
-		js.put("Calling Name", "Jaganathann");
+		js.put("Calling Name", "Jaganathannn");
 		js.put("DOB", "08-Dec-2020");
 		js.put("Hobbies", "Reading Techical Blogss");
 		js.put("Places he like", "His nativeee");
